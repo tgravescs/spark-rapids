c6abc6613c924811c778a4055ef3706f16cd1cea
Thomas Graves <tgraves@nvidia.com>
2020-12-02 16:22:25 -0600

    remove logs

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuParquetScan.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuParquetScan.scala
@@ -1023,7 +1023,7 @@ class MultiFileParquetPartitionReader(
             // logWarning("concatenating different partition values")
             var succeeded = false
             val numCols = allPartitionColumns.head.size
-            logWarning("num cols is: " + numCols)
+            // logWarning("num cols is: " + numCols)
             val result = new Array[GpuColumnVector](numCols)
             try {
               for (i <- result.indices) {
@@ -1050,7 +1050,7 @@ class MultiFileParquetPartitionReader(
               val resultCols = fileBatchCols ++ result
               val finalCb = new ColumnarBatch(resultCols.toArray, cb.numRows)
               fileBatchCols.foreach(_.asInstanceOf[GpuColumnVector].incRefCount())
-              logWarning("after inc ref count")
+              // logWarning("after inc ref count")
               succeeded = true
               finalCb
             } finally {
