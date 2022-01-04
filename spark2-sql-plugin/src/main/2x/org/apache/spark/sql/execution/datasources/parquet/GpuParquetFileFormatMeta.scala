4e3b48d7416a9bdcc6c81a7e6b268c325005b6ea
Thomas Graves <tgraves@nvidia.com>
2022-01-04 14:55:20 -0600

    Put spark2 as separate module
    Signed-off-by: Thomas Graves <tgraves@nvidia.com>

---
+++ spark2-sql-plugin/src/main/2x/org/apache/spark/sql/execution/datasources/parquet/GpuParquetFileFormatMeta.scala
@@ -0,0 +1,254 @@
+/*
+ * Copyright (c) 2019-2021, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.datasources.parquet
+
+import ai.rapids.cudf._
+import com.nvidia.spark.rapids._
+import org.apache.hadoop.mapreduce.{Job, OutputCommitter, TaskAttemptContext}
+import org.apache.parquet.hadoop.{ParquetOutputCommitter, ParquetOutputFormat}
+import org.apache.parquet.hadoop.ParquetOutputFormat.JobSummaryLevel
+import org.apache.parquet.hadoop.codec.CodecConfig
+import org.apache.parquet.hadoop.util.ContextUtil
+
+import org.apache.spark.internal.Logging
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.internal.SQLConf
+import org.apache.spark.sql.internal.SQLConf.ParquetOutputTimestampType
+import org.apache.spark.sql.rapids.execution.TrampolineUtil
+import org.apache.spark.sql.types._
+
+object GpuParquetFileFormatMeta {
+  def tagGpuSupport(
+      meta: RapidsMeta[_, _],
+      spark: SparkSession,
+      options: Map[String, String],
+      schema: StructType): Option[GpuParquetFileFormat] = {
+
+    val sqlConf = spark.sessionState.conf
+    val parquetOptions = new ParquetOptions(options, sqlConf)
+
+    if (!meta.conf.isParquetEnabled) {
+      meta.willNotWorkOnGpu("Parquet input and output has been disabled. To enable set" +
+        s"${RapidsConf.ENABLE_PARQUET} to true")
+    }
+
+    if (!meta.conf.isParquetWriteEnabled) {
+      meta.willNotWorkOnGpu("Parquet output has been disabled. To enable set" +
+        s"${RapidsConf.ENABLE_PARQUET_WRITE} to true")
+    }
+
+    FileFormatChecks.tag(meta, schema, ParquetFormatType, WriteFileOp)
+
+    parseCompressionType(parquetOptions.compressionCodecClassName)
+      .getOrElse(meta.willNotWorkOnGpu(
+        s"compression codec ${parquetOptions.compressionCodecClassName} is not supported"))
+
+    if (sqlConf.writeLegacyParquetFormat) {
+      meta.willNotWorkOnGpu("Spark legacy format is not supported")
+    }
+
+    if (!meta.conf.isParquetInt96WriteEnabled && sqlConf.parquetOutputTimestampType ==
+      ParquetOutputTimestampType.INT96) {
+      meta.willNotWorkOnGpu(s"Writing INT96 is disabled, if you want to enable it turn it on by " +
+        s"setting the ${RapidsConf.ENABLE_PARQUET_INT96_WRITE} to true. NOTE: check " +
+        "out the compatibility.md to know about the limitations associated with INT96 writer")
+    }
+
+    val schemaHasTimestamps = schema.exists { field =>
+      TrampolineUtil.dataTypeExistsRecursively(field.dataType, _.isInstanceOf[TimestampType])
+    }
+    if (schemaHasTimestamps) {
+      if(!isOutputTimestampTypeSupported(sqlConf.parquetOutputTimestampType)) {
+        meta.willNotWorkOnGpu(s"Output timestamp type " +
+          s"${sqlConf.parquetOutputTimestampType} is not supported")
+      }
+    }
+
+    val schemaHasDates = schema.exists { field =>
+      TrampolineUtil.dataTypeExistsRecursively(field.dataType, _.isInstanceOf[DateType])
+    }
+
+
+    ShimLoader.getSparkShims.int96ParquetRebaseWrite(sqlConf) match {
+      case "EXCEPTION" =>
+      case "CORRECTED" =>
+      case "LEGACY" =>
+        if (schemaHasTimestamps) {
+          meta.willNotWorkOnGpu("LEGACY rebase mode for int96 timestamps is not supported")
+        }
+      case other =>
+        meta.willNotWorkOnGpu(s"$other is not a supported rebase mode for int96")
+    }
+
+    ShimLoader.getSparkShims.parquetRebaseWrite(sqlConf) match {
+      case "EXCEPTION" => //Good
+      case "CORRECTED" => //Good
+      case "LEGACY" =>
+        if (schemaHasDates || schemaHasTimestamps) {
+          meta.willNotWorkOnGpu("LEGACY rebase mode for dates and timestamps is not supported")
+        }
+      case other =>
+        meta.willNotWorkOnGpu(s"$other is not a supported rebase mode")
+    }
+
+    if (meta.canThisBeReplaced) {
+      Some(new GpuParquetFileFormat)
+    } else {
+      None
+    }
+  }
+
+  def parseCompressionType(compressionType: String): Option[CompressionType] = {
+    compressionType match {
+      case "NONE" | "UNCOMPRESSED" => Some(CompressionType.NONE)
+      case "SNAPPY" => Some(CompressionType.SNAPPY)
+      case _ => None
+    }
+  }
+
+  def isOutputTimestampTypeSupported(
+     outputTimestampType: ParquetOutputTimestampType.Value): Boolean = {
+    outputTimestampType match {
+      case ParquetOutputTimestampType.TIMESTAMP_MICROS |
+           ParquetOutputTimestampType.TIMESTAMP_MILLIS |
+           ParquetOutputTimestampType.INT96 => true
+      case _ => false
+    }
+  }
+}
+
+class GpuParquetFileFormat extends ColumnarFileFormat with Logging {
+  /**
+   * Prepares a write job and returns an [[ColumnarOutputWriterFactory]].  Client side job
+   * preparation can be put here.  For example, user defined output committer can be configured
+   * here by setting the output committer class in the conf of
+   * spark.sql.sources.outputCommitterClass.
+   */
+  @scala.annotation.nowarn(
+    "msg=value ENABLE_JOB_SUMMARY in class ParquetOutputFormat is deprecated"
+  )
+  override def prepareWrite(
+      sparkSession: SparkSession,
+      job: Job,
+      options: Map[String, String],
+      dataSchema: StructType): ColumnarOutputWriterFactory = {
+    val parquetOptions = new ParquetOptions(options, sparkSession.sessionState.conf)
+
+    val conf = ContextUtil.getConfiguration(job)
+
+    val outputTimestampType = sparkSession.sessionState.conf.parquetOutputTimestampType
+    val dateTimeRebaseException = "EXCEPTION".equals(
+      sparkSession.sqlContext.getConf(ShimLoader.getSparkShims.parquetRebaseWriteKey))
+    // prior to spark 311 int96 don't check for rebase exception
+    // https://github.com/apache/spark/blob/068465d016447ef0dbf7974b1a3f992040f4d64d/sql/core/src/
+    // main/scala/org/apache/spark/sql/execution/datasources/parquet/ParquetWriteSupport.scala#L195
+    val hasSeparateInt96RebaseConf = ShimLoader.getSparkShims.hasSeparateINT96RebaseConf
+    val timestampRebaseException =
+      outputTimestampType.equals(ParquetOutputTimestampType.INT96) &&
+          "EXCEPTION".equals(sparkSession.sqlContext
+              .getConf(ShimLoader.getSparkShims.int96ParquetRebaseWriteKey)) &&
+          hasSeparateInt96RebaseConf ||
+          !outputTimestampType.equals(ParquetOutputTimestampType.INT96) && dateTimeRebaseException
+
+    val committerClass =
+      conf.getClass(
+        SQLConf.PARQUET_OUTPUT_COMMITTER_CLASS.key,
+        classOf[ParquetOutputCommitter],
+        classOf[OutputCommitter])
+
+    if (conf.get(SQLConf.PARQUET_OUTPUT_COMMITTER_CLASS.key) == null) {
+      logInfo("Using default output committer for Parquet: " +
+          classOf[ParquetOutputCommitter].getCanonicalName)
+    } else {
+      logInfo("Using user defined output committer for Parquet: " +
+        committerClass.getCanonicalName)
+    }
+
+    conf.setClass(
+      SQLConf.OUTPUT_COMMITTER_CLASS.key,
+      committerClass,
+      classOf[OutputCommitter])
+
+    // We're not really using `ParquetOutputFormat[Row]` for writing data here.  The reason why
+    // we set it here is to setup the output committer class to `ParquetOutputCommitter`, which is
+    // bundled with `ParquetOutputFormat[Row]`.
+    job.setOutputFormatClass(classOf[ParquetOutputFormat[Row]])
+
+    ParquetOutputFormat.setWriteSupportClass(job, classOf[ParquetWriteSupport])
+
+    // This metadata is useful for keeping UDTs like Vector/Matrix.
+    ParquetWriteSupport.setSchema(dataSchema, conf)
+
+    if (sparkSession.sessionState.conf.writeLegacyParquetFormat) {
+      throw new UnsupportedOperationException("Spark legacy output format not supported")
+    }
+    // Sets flags for `ParquetWriteSupport`, which converts Catalyst schema to Parquet
+    // schema and writes actual rows to Parquet files.
+    conf.set(
+      SQLConf.PARQUET_WRITE_LEGACY_FORMAT.key,
+      sparkSession.sessionState.conf.writeLegacyParquetFormat.toString)
+
+    if(!GpuParquetFileFormat.isOutputTimestampTypeSupported(outputTimestampType)) {
+      val hasTimestamps = dataSchema.exists { field =>
+        TrampolineUtil.dataTypeExistsRecursively(field.dataType, _.isInstanceOf[TimestampType])
+      }
+      if (hasTimestamps) {
+        throw new UnsupportedOperationException(
+          s"Unsupported output timestamp type: $outputTimestampType")
+      }
+    }
+    conf.set(SQLConf.PARQUET_OUTPUT_TIMESTAMP_TYPE.key, outputTimestampType.toString)
+
+    // Sets compression scheme
+    conf.set(ParquetOutputFormat.COMPRESSION, parquetOptions.compressionCodecClassName)
+
+    val compressionType =
+      GpuParquetFileFormat.parseCompressionType(parquetOptions.compressionCodecClassName)
+        .getOrElse(
+          throw new UnsupportedOperationException(
+            s"compression codec ${parquetOptions.compressionCodecClassName} is not supported"))
+
+    // SPARK-15719: Disables writing Parquet summary files by default.
+    if (conf.get(ParquetOutputFormat.JOB_SUMMARY_LEVEL) == null
+        && conf.get(ParquetOutputFormat.ENABLE_JOB_SUMMARY) == null) {
+      conf.setEnum(ParquetOutputFormat.JOB_SUMMARY_LEVEL, JobSummaryLevel.NONE)
+    }
+
+    if (ParquetOutputFormat.getJobSummaryLevel(conf) != JobSummaryLevel.NONE
+        && !classOf[ParquetOutputCommitter].isAssignableFrom(committerClass)) {
+      // output summary is requested, but the class is not a Parquet Committer
+      logWarning(s"Committer $committerClass is not a ParquetOutputCommitter and cannot" +
+          s" create job summaries. " +
+          s"Set Parquet option ${ParquetOutputFormat.JOB_SUMMARY_LEVEL} to NONE.")
+    }
+
+    new ColumnarOutputWriterFactory {
+        override def newInstance(
+          path: String,
+          dataSchema: StructType,
+          context: TaskAttemptContext): ColumnarOutputWriter = {
+        new GpuParquetWriter(path, dataSchema, compressionType, dateTimeRebaseException,
+          timestampRebaseException, context)
+      }
+
+      override def getFileExtension(context: TaskAttemptContext): String = {
+        CodecConfig.from(context).getCodec.getExtension + ".parquet"
+      }
+    }
+  }
+}
+
