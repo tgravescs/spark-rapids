55ec83879d6f08c43add637d58a84e9b3af5e6d5
Thomas Graves <tgraves@nvidia.com>
2022-10-31 13:48:07 -0500

    fix test

--- tests/src/test/scala/com/nvidia/spark/rapids/GpuMultiFileReaderSuite.scala
+++ tests/src/test/scala/com/nvidia/spark/rapids/GpuMultiFileReaderSuite.scala
@@ -32,7 +32,7 @@ class GpuMultiFileReaderSuite extends FunSuite with Arm {
   test("avoid infinite loop when host buffers empty") {
     val conf = new Configuration(false)
     val membuffers =
-      Array(HostMemoryBufferInfo(HostMemoryBuffer.allocate(0), 0L, 0, Seq.empty, null))
+      Array(HostMemoryBufferInfo(HostMemoryBuffer.allocate(0), 0L, 0, Seq.empty, null, 0))
     val multiFileReader = new MultiFileCloudPartitionReaderBase(
       conf,
       inputFiles = Array.empty,
