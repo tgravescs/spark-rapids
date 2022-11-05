a96202989a643bf667e7b7295f93e764a122a943
Thomas Graves <tgraves@apache.org>
2022-11-04 20:12:46 -0500

    fix iceberg

--- sql-plugin/src/main/java/com/nvidia/spark/rapids/iceberg/spark/source/GpuMultiFileBatchReader.java
+++ sql-plugin/src/main/java/com/nvidia/spark/rapids/iceberg/spark/source/GpuMultiFileBatchReader.java
@@ -329,7 +329,7 @@ class GpuMultiFileBatchReader extends BaseDataReader<ColumnarBatch> {
       );
     }
 
-    private ParquetFileInfoWithBlockMeta filterParquetBlocks(PartitionedFile file) {
+    private ParquetFileInfoWithBlockMeta filterParquetBlocks(PartitionedFile file, org.apache.parquet.io.InputFile inputFile) {
       FileScanTask fst = files.get(file.filePath());
       FilteredParquetFileInfo filteredInfo = filterParquetBlocks(fst);
       constsSchemaMap.put(file.filePath(),
