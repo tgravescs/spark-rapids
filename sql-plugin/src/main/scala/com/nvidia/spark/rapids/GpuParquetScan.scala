4f44f44b3d6727a4e5a03d8b8a9013d22d9f375a
Thomas Graves <tgraves@apache.org>
2023-04-20 10:56:25 -0500

    fail int96 there

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuParquetScan.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuParquetScan.scala
@@ -714,12 +714,11 @@ private case class GpuParquetFileFilterHandler(@transient sqlConf: SQLConf) exte
         GpuParquetPartitionReaderFactoryBase.isCorrectedInt96RebaseMode(
           footer.getFileMetaData.getKeyValueMetaData.get, isInt96CorrectedRebase)
 
-     /* if (isCorrectedInt96RebaseForThisFile == false) {
-
-        throw new Exception(s"is isCorrectedInt96RebaseForThisFile: $isCorrectedInt96RebaseForThisFile " +
+     if (isCorrectedInt96RebaseForThisFile == false && hasInt96Timestamps == true) {
+        throw new Exception(s"is isCorrectedInt96RebaseForThisFile and has int96: $isCorrectedInt96RebaseForThisFile " +
           s"CONFIG $isInt96CorrectedRebase file meta is: " +
           s"file is $file")
-      }*/
+      }
 
       val blocks = if (pushedFilters.isDefined) {
         withResource(new NvtxRange("getBlocksWithFilter", NvtxColor.CYAN)) { _ =>
